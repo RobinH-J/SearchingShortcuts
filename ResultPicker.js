ec9cfd6ab7c0926d83018b478140407ecb0a7cc1
Brad Cupit <106213+bradcupit@users.noreply.github.com>
2022-03-03 16:43:05 -0500

    support child links (#3)
    Example google search: npm package
    
    The first link result is to npmjs.org, but then there are 6 child links, one to "About npm" and one to "Sign In", etc.
    Previous versions of the code skipped over all of them. Now we don't.
    
    Also refactored to use 4 spaces consistently.
    Renamed some locals for clarity.

--- ResultPicker.js
+++ ResultPicker.js
@@ -8,51 +8,66 @@
 //     </a>
 //   </div>
 // And sometimes we get pseudo-results (like Ads) that don't contain the h3: exclude them.
-const querySelector = 'div.yuRUbf h3';
-document.selectedResultId = 0;
+//
+// The comma is an OR statement. Sometimes the first result is to a page like npmjs.org, then there's 6 child page links under it, like "About npm" and "Sign In".
+// Search for this to see what we mean: npm package
+// Simplified Google html:
+//   <div class="usJj9c">
+//     <h3>
+//       <a class="l" href="The real url is here">The big text we see in Google Search results</a>
+//     </h3>
+//    </div>
+const querySelector = 'div.yuRUbf a h3, div.usJj9c h3 a';
+const searchResultsArray = Array.from(document.querySelectorAll(querySelector)).filter(node =>
+    // Filter out "People also ask" section.
+    // Links in that section aren't initially shown, so skip them
+    // Queries to test with:
+    //   "who is the president" displays the 'People also ask' section
+    //   "npm create package" displays a 'featured snippet' + link above the main results (we want that link to get selected)
+    //   "what is an apple" displays a link, then the 'People also ask' section
+    !node.closest('.Wt5Tfe')
+);
+document.selectedIndexInSearchResultsArray = 0;
 
-function selectResult(newId) {
-    let linkTextArray = getSearchResultsAsArray();
-    if (newId < 0 || newId >= linkTextArray.length) {
-        return  // future idea: modify for next/prev page
-    }
-
-    document.selectedResultId = newId;
-    let linkText = linkTextArray[newId];
-    let link = linkText.parentElement;
+function selectResult(newIndex) {
+    document.selectedIndexInSearchResultsArray = newIndex;
+    let link = findLinkByIndex(newIndex);
     link.focus();
 }
 
-function getSearchResultsAsArray() {
-    return Array.from(document.querySelectorAll(querySelector)).filter(node =>
-      // Filter out "People also ask" section.
-      // Links in that section aren't initially shown, so skip them
-      // Queries to test with:
-      //   "who is the president" displays the 'People also ask' section
-      //   "npm create package" displays a 'featured snippet' + link above the main results (we want that link to get selected)
-      //   "what is an apple" displays a link, then the 'People also ask' section
-      !node.closest('.Wt5Tfe')
-    );
+function findLinkByIndex(newIndex) {
+    if (newIndex < 0 || newIndex >= searchResultsArray.length) {
+        return  // future idea: modify for next/prev page
+    }
+
+    let searchResult = searchResultsArray[newIndex];
+
+    if (searchResult.parentNode.nodeName.toLowerCase() === 'a') {
+        return searchResult.parentNode;
+    } else if (searchResult.nodeName.toLowerCase() === 'a') {
+        return searchResult;
+    } else {
+        throw 'Expected an <a> with an embedded <h3> or an <h3> with an embedded <a> but instead got ' + searchResult;
+    }
 }
 
 document.onkeydown = function(event) {
     if (event.key === 'ArrowUp' || isVimUpKey(event.key)) {
-        selectResult(document.selectedResultId - 1);
+        selectResult(document.selectedIndexInSearchResultsArray - 1);
     }
 
     if (event.key === 'ArrowDown' || isVimDownKey(event.key)) {
-        selectResult(document.selectedResultId + 1);
+        selectResult(document.selectedIndexInSearchResultsArray + 1);
     }
 
     if (event.key === 'Enter') {
-      let linkText = getSearchResultsAsArray()[document.selectedResultId];
-      let link = linkText.parentElement;
-      let url = link.href;
-      if (event.metaKey) {
-          window.open(url, '_blank');
-      } else {
-          document.location = url;
-      }
+        let link = findLinkByIndex(document.selectedIndexInSearchResultsArray);
+        let url = link.href;
+        if (event.metaKey) {
+            window.open(url, '_blank');
+        } else {
+            document.location = url;
+        }
     }
 }
 
